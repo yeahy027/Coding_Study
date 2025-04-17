5878752d27e9b37f9afce0a90f27ab49f087f6e3
yeahy027 <yeahy027@naver.com>
2025-04-17 20:40:42 +0900

    [Silver III] Title: 소수 구하기, Time: 1220 ms, Memory: 33784 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int count = 0;
+
+        for (int i = M; i <= N; i++) {
+            if (i < 2) continue;
+
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
