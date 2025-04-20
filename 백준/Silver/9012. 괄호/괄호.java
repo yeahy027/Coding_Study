fa1a00c35ebad2446d697114a378c458fb2ce828
yeahy027 <yeahy027@naver.com>
2025-04-20 18:53:49 +0900

    [Silver IV] Title: 괄호, Time: 192 ms, Memory: 18108 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public String solution(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        Main m = new Main();
+
+        for (int i = 0; i < n; i++) {
+            String input = sc.nextLine();
+            System.out.println(m.solution(input));
+        }
+    }
+}
