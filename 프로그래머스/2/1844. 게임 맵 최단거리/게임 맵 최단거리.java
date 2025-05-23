f5d84938aa75c3cf12ee220c0b12e4e8e32c6974
yeahy027 <yeahy027@naver.com>
2025-05-23 14:25:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.03 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,44 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        // BFS로 최단거리 구하기
         
-        // 시작점 예약 (0, 0, 거리 1)
-        int[] dr = {1, -1, 0, 0};
-        int[] dc = {0, 0, 1, -1};
+        Queue<int[]> queue = new ArrayDeque<>();
         
         int n = maps.length;
         int m = maps[0].length;
-        Queue<int[]> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
         
-        // 배열은 출력 시 toString 사용
-        // 2차원 배열은 출력 시 deepToString 사용
-        // System.out.println(Arrays.deepToString(visited));
-        queue.offer(new int[]{0, 0, 1});
+        // 이동 방향
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, 1, -1};
+        
+        // 시작점 : 좌표(0, 0), 이동한 칸의 개수를 1로 기본 설정
+        queue.offer(new int[] {0, 0, 1});
         visited[0][0] = true;
         
         while (!queue.isEmpty()) {
-            // 현재 노드 방문
+            // 현재 노드
             int[] cur = queue.poll();
-            // System.out.println(Arrays.toString(cur));
             
+            // 현재 좌표와 이동한 칸의 개수
             int r = cur[0], c = cur[1], dist = cur[2];
             
-            // if 도착지 도달 시 return dist;
-            if(r == n - 1 && c == m - 1) return dist;
+            // 목적지 도착 : (0, 0)부터 시작해서 (n - 1, m - 1)가 끝
+            if (r == n - 1 && c == m - 1) return dist;
             
-            // 다음 노드 예약
+            // 다음 노드 : 네 가지 방향이기 때문에 i < 4로 범위 설정
             for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i], nc = c + dc[i];
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                // 이동할 좌표가 맵 내에 존재하고, 벽이 없는 자리일 때
+                if (0 <= nr && nr < n && 0 <= nc && nc < m && maps[nr][nc] == 1) {
                     if (!visited[nr][nc]) {
-                        queue.offer(new int[]{nr, nc, dist + 1});
+                        queue.offer(new int[] {nr, nc, dist + 1});
                         visited[nr][nc] = true;
                     }
-                } 
+                }
             }
-            
         }
         
         return -1;
