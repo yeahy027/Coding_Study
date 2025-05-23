7757ab2ccee165010819d23017fe447a69f53c42
yeahy027 <yeahy027@naver.com>
2025-05-23 10:06:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.12 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] maps) {
+        // BFS로 최단거리 구하기
+        
+        // 시작점 예약 (0, 0, 거리 1)
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, 1, -1};
+        
+        int n = maps.length;
+        int m = maps[0].length;
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[n][m];
+        
+        // 배열은 출력 시 toString 사용
+        // 2차원 배열은 출력 시 deepToString 사용
+        // System.out.println(Arrays.deepToString(visited));
+        queue.offer(new int[]{0, 0, 1});
+        visited[0][0] = true;
+        
+        while (!queue.isEmpty()) {
+            // 현재 노드 방문
+            int[] cur = queue.poll();
+            // System.out.println(Arrays.toString(cur));
+            
+            int r = cur[0], c = cur[1], dist = cur[2];
+            
+            // if 도착지 도달 시 return dist;
+            if(r == n - 1 && c == m - 1) return dist;
+            
+            // 다음 노드 예약
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i], nc = c + dc[i];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1) {
+                    if (!visited[nr][nc]) {
+                        queue.offer(new int[]{nr, nc, dist + 1});
+                        visited[nr][nc] = true;
+                    }
+                } 
+            }
+            
+        }
+        
+        return -1;
+    }
+}
