7802cb09b5fd6f152745d546839a97109cad7a0e
yeahy027 <yeahy027@naver.com>
2025-06-19 16:37:16 +0900

    [Silver V] Title: K번째 수, Time: 3240 ms, Memory: 461400 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.get(K - 1));
+    }
+}
