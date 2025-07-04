1776343500a83152bf536e23f7426948c295a61d
yeahy027 <yeahy027@naver.com>
2025-07-04 13:13:32 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 13.77 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,27 +3,25 @@ import java.util.*;
 class Solution {
     
     class Job {
-        int s;
-        int l;
+        int start;
+        int length;
         
-        Job(int s, int l) {
-            this.s = s;
-            this.l = l;
+        Job(int start, int length) {
+            this.start = start;
+            this.length = length;
         }
     }
     
     public int solution(int[][] jobs) {
         
-        // 요청 시간 기준으로 정렬
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
         
-        // 우선순위 큐 : 작업 소요 시간이 짧은 순
-        PriorityQueue<Job> pq = new PriorityQueue<>((a, b) -> a.l - b.l);
+        PriorityQueue<Job> pq = new PriorityQueue<>((a, b) -> a.length - b.length);
         
-        int time = 0;       // 현재 시간
-        int idx = 0;        // jobs 배열 인덱스
-        int totalTime = 0;  // 총 대기 시간
-        int count = 0;      // 처리된 작업 수
+        int time = 0;
+        int idx = 0;
+        int totalTime = 0;
+        int count = 0;
         
         while (count < jobs.length) {
             while (idx < jobs.length && jobs[idx][0] <= time) {
@@ -33,8 +31,8 @@ class Solution {
             
             if (!pq.isEmpty()) {
                 Job cur = pq.poll();
-                time += cur.l;
-                totalTime += time - cur.s;
+                time += cur.length;
+                totalTime += time - cur.start;
                 count++;
             } else {
                 time = jobs[idx][0];
