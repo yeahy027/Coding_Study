655a616e37e46695df1b568a999ecf6d6c0c9088
Yeyoung Kim <yeahy027@naver.com>
2026-03-30 22:58:15 +0900

    [level 2] Title: 피로도, Time: 3.85 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    
+    static int answer = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        
+        dfs(k, dungeons, visited, 0);
+        
+        return answer;
+    }
+    
+    public void dfs(int k, int[][] dungeons, boolean[] visited, int count) {
+        
+        answer = Math.max(answer, count);
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, visited, count + 1);
+                
+                // 백트래킹
+                visited[i] = false;
+            }
+        }    
+    }
+}
